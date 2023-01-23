2b79ee15f8d412672507c0829dafbff0725974b3
Ketanbansode <121768566+Ketanbansode@users.noreply.github.com>
2023-01-23 18:06:01 +0530

    Update q-2Simplyfi.py

--- q-2Simplyfi.py
+++ q-2Simplyfi.py
@@ -1,15 +1,14 @@
-n=int(input("Enter test cases"))
+n=int(input("Enter test cases:"))
 arr=[]
-while(n==0):
-    x=int(input("Total no. of players between Gihun And Ali"))
-    y=int(input("Enter the height of both them"))
+for j in range(n):
+    x=int(input("Total no. of players between Gihun And Ali:"))
+    y=int(input("Enter the height of both them:"))
     count=0
     for i in range (x):
-        arr.append(int(input("enter the height of ith person",i)))
+        arr.append(int(input("enter the height of ith person:")))
         if (arr[i]>y):
             count+=1
-    n-=1
-    print(count)
+    print("minimum no. of player to get shot",count)
 
 
 
